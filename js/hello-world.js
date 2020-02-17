70054786c9bb946b7596b773c8b950b9ddde9d35
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-02-17 12:31:34 -0600

    Hello World function completed

---
+++ js/hello-world.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+/**
+ *   Create a function that takes in a variable and outputs 'Hello, ' plus the variable
+ *   For example: 'Hello, World!'
+ **/
+
+function greeting(name) {
+
+    // Return 'Hello, ' concat '+' the variable 'name'
+    return 'Hello, ' + name;
+
+}
+
+console.log(greeting("World"));
